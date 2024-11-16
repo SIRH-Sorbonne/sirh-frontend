1b287e6c92f4d6f76f3323a852a5688107b7484c
LViogeat <louis.viogeat@gmail.com>
2024-11-16 11:00:57 +0100

    add other themes

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'  // Importez le thème Aura (ou un autre thème de votre choix)
+import Lara from '@primevue/themes/lara'  // Importez le thème Lara (ou un autre thème de votre choix)
+import Material from '@primevue/themes/material'  // Importez le thème Material (ou un autre thème de votre choix)
+import Nora from '@primevue/themes/nora'  // Importez le thème Nora (ou un autre thème de votre choix)
 import 'primeicons/primeicons.css'
 import Menubar from 'primevue/menubar';
 
